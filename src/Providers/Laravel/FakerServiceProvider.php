9a3aeb73a3ae7a7c8fe194d1841d65b4fe560dca
davarch <tim@davarch.ru>
2023-11-04 23:22:34 +0500

    fix: Fixed Laravel provider add faker

--- src/Providers/Laravel/FakerServiceProvider.php
+++ src/Providers/Laravel/FakerServiceProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Davarch\FakerRussian\Providers\Laravel;
 
 use Davarch\FakerRussian\Faker;
+use Davarch\FakerRussian\Providers\FakerProvider;
+use Faker\Factory;
 use Faker\Generator;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,6 +14,11 @@ final class FakerServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(Generator::class, Faker::class);
+        $this->app->singleton(Generator::class.':ru_RU', function () {
+            $faker = Factory::create();
+            $faker->addProvider(new FakerProvider($faker));
+
+            return $faker;
+        });
     }
 }
